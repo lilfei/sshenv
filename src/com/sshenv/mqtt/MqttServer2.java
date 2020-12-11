ca9a8c69e991d32c65bc8e68903e10fd3a882feb
Satvr <satvr@126.com>
2020-12-11 14:43:08 +0800

    mqtt,配合本机activemq

---
+++ src/com/sshenv/mqtt/MqttServer2.java
@@ -0,0 +1,66 @@
+package com.sshenv.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+/**
+ * mqtt test
+ * @author llf
+ * @date 2020年12月11日
+ */
+public class MqttServer2 {
+    /**
+     * 代理服务器ip地址
+     */
+    public static final String MQTT_BROKER_HOST = "tcp://127.0.0.1:61613";
+
+    /**
+     * 订阅标识
+     */
+    public static final String MQTT_TOPIC = "test2";
+
+    private static String userName = "admin";
+    private static String password = "password";
+
+    /**
+     * 客户端唯一标识
+     */
+    public static final String MQTT_CLIENT_ID = "android_server_xiasuhuei32";
+    private static MqttTopic topic;
+    private static MqttClient client;
+
+    public static void main(String... args) {
+        // 推送消息
+        MqttMessage message = new MqttMessage();
+        try {
+            client = new MqttClient(MQTT_BROKER_HOST, MQTT_CLIENT_ID, new MemoryPersistence());
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            options.setUserName(userName);
+            options.setPassword(password.toCharArray());
+            options.setConnectionTimeout(10);
+            options.setKeepAliveInterval(20);
+
+            topic = client.getTopic(MQTT_TOPIC);
+
+            message.setQos(1);
+            message.setRetained(false);
+            message.setPayload("message from server222222".getBytes());
+            client.connect(options);
+
+            while (true) {
+                MqttDeliveryToken token = topic.publish(message);
+                token.waitForCompletion();
+                System.out.println("已经发送222");
+                Thread.sleep(10000);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
